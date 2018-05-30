272f04623c26cbe3adae20c8b69d5c46ad04bb1d
Kameron Tanseli <kamerontanseliwork@gmail.com>
2018-05-30 19:41:34 +0100

    Hope

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,10 @@ git add -A
 git commit
 git push
 git push origin testing:master -f
+
+git checkout master
+ls | grep -v www | xarg rm -r
+mv www/* .
+git add -A
+git commit -m "deploying"
+git push -f
