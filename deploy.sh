efa50fec562bb1add4b7e3aa90ca154850af127e
Kameron Tanseli <kamerontanseliwork@gmail.com>
2018-05-30 21:09:00 +0100

    changes

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 harp compile .
 rm www/deploy.sh
 git add -A
-git commit -m "deploying"
+git commit
 git push
 git push origin testing:master -f
 
 git checkout master
-ls
+ls | grep -v www | xargs rm -r
+mv www/* .
+rm -r www
