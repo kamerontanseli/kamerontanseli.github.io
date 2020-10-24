19aae5e589b0824c6b040123bfcef918f9d678ee
Kameron Tanseli <kamerontanseliwork@gmail.com>
2020-10-24 11:45:45 +0100

    projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -16,10 +16,42 @@ const Projects = () => (
         <div className="projects">
           <div className="projects-project">
             <h3>
-              <a target="_blank" href="https://londonremotework.com/">
-                londonremotework.com
+              <a target="_blank" href="https://dreamline.netlify.app">
+                4HWW Dreamline Tool
               </a>
             </h3>
+            <p>
+              The 4 Hour Work Week is one of my all time favourite books. Having
+              gone from working 9-5s to 4 hour days myself it's been a huge
+              inspiration to me for years.
+            </p>
+            <p>
+              I commonly use the dreamline worksheet in the book to map out my
+              goals in life every quarter. I realised I could just make it into
+              an offline-first web app 🙃.
+            </p>
+          </div>
+          <div className="projects-project">
+            <h3>
+              <a
+                target="_blank"
+                href="https://chrome.google.com/webstore/detail/meta-scan/eccncioknliednbpdblekghbbchbklip"
+              >
+                getmetascan.com
+              </a>
+            </h3>
+            <p>
+              Having not found a good meta tag analysis extension. I decided to
+              build my own.
+            </p>
+            <p>
+              Run a validation on the top 13 key meta tags needed for SEO/Social
+              and instantly preview what the URL your on will look like on
+              Twitter, Google, and Facebook.
+            </p>
+          </div>
+          <div className="projects-project">
+            <h3>londonremotework.com</h3>
             <p>
               In the midst of a nationwide lockdown and increasing uneployment.
               I decided to use tray.io and Next.js to create an ad-free job site
@@ -27,11 +59,7 @@ const Projects = () => (
             </p>
           </div>
           <div className="projects-project">
-            <h3>
-              <a target="_blank" href="https://tweet2gram.com/">
-                tweet2gram.com
-              </a>
-            </h3>
+            <h3>tweet2gram.com</h3>
             <p>
               Just another twitter screenshot tool. P.S: I loved getting cease
               and decist emails from Instagram.
@@ -54,6 +82,9 @@ const Projects = () => (
         display: grid;
         grid-template-columns: repeat(3, 1fr);
         grid-gap: 20px;
+      }
+      .projects-project h3 {
+        margin-top: 0;
       }
       .projects-project h3 a {
         color: #0073a7;
