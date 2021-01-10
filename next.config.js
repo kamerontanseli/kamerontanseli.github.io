f132e85d85c81a390774916bc074f1f18e64a38e
Kameron Tanseli <kamerontanseliwork@gmail.com>
2021-01-10 13:47:49 +0000

    iki

--- next.config.js
+++ next.config.js
@@ -13,7 +13,6 @@ module.exports = ({
       "/": { page: "/" },
       "/projects": { page: "/projects" },
       "/metrics": { page: "/metrics" },
-      "/clean-meal-generator": { page: "/clean-meal-generator" },
     };
     //get all .md files in the posts dir
     const blogs = glob.sync('src/posts/**/*.md')
